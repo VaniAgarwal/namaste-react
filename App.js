ddd96915a7a1d1cd5d847f8c0a8eaaf95238e713
Vani <vani.gupta031992@gmail.com>
2023-06-01 17:37:27 +0530

    Episode 2 and 3

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+ import React from "react";
+ import reactDom from "react-dom/client";
+ 
  /**
   * <div id="parent">
     * <div id ="child1">
@@ -17,23 +20,77 @@
  //so to ease this we use jsx
  
  // <!--pure react thing-->
- const parent = React.createElement('div',{id:"parent"},[
-    React.createElement('div',{id:"child1"},[
-        React.createElement('h1',{},'I, am H1 tag'),
-        React.createElement('h2',{},'I, am h2 tag')
-    ]),
-    React.createElement('div',{id:"child2"},[
-        React.createElement('h1',{},'I am child2 h2 tag'),
-        React.createElement('h2',{},'I am child2 h2 tag')
-    ]),
+//  const parent = React.createElement('div',{id:"parent"},[
+//     React.createElement('div',{id:"child1"},[
+//         React.createElement('h1',{},'I, am H1 tag'),
+//         React.createElement('h2',{},'I, am h2 tag')
+//     ]),
+//     React.createElement('div',{id:"child2"},[
+//         React.createElement('h1',{},'I am child2 h2 tag'),
+//         React.createElement('h2',{},'I am child2 h2 tag')
+//     ]),
 
- ]);
+//  ]);
 
- console.log(parent);
+//  console.log(parent);
 
 //  console.log(heading);//returns an object
 
  //when we want to put inside the browser or we want to render in browser we use DOM
 
- const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(parent);
+//  const root = reactDom.createRoot(document.getElementById('root'));
+//  root.render(parent);
+
+//react element
+const heading = (
+<h1>Namaste React using JSX</h1>
+);
+
+//React Functional COmponent
+const Heading= ()=>{
+  return <h1 className="headingf" tabIndex="5">Functional Componenet in React</h1>
+};
+
+//This is also Functional Component
+const HeadingComponent = ()=> (
+  <div id="container">
+   {Heading()} {/* we can also write a component like this */}
+
+  {/* It is also called component composition */}
+  <Heading/>
+  <Heading></Heading>
+  {/* if i have to use javascript in a component by using {} */}
+  {heading}
+  <h1>Function Componenet 2</h1>
+  <ReactFragment></ReactFragment>
+  </div>
+  
+)
+
+/*React Fragments are used to....suppose if use div we can only have one parent element so we use all the code inside a div...
+so to actually avoid this because of ugliness we use <React.Fragment> or <></>. So the extra div will not show in the browser.
+*/
+const ReactFragment = ()=>(
+  <React.Fragment>
+  <div>
+    <h1>React Fragment</h1>
+  </div>
+
+
+  </React.Fragment>
+)
+
+//We can also write as a normal Javascript function but we use arrow functions in industry
+const FunctionalComponenet = function(){
+  return(
+    <div>
+      <h1>Functional Component using functions</h1>
+    </div>
+  )
+}
+
+
+
+const root = reactDom.createRoot(document.getElementById('root'));
+// root.render(heading)
+root.render(<HeadingComponent/>)
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-Namaste-React
+# Namaste-React 
+
+
+# parcel-Read parcel Documentation
+-Dev build
+-local server
+-HMR-Hot Module Replacement
+-It is automatically refreshing the page while saving the changes with the help of "File Watching Algorithm" written in c++.Parcel is keeping a track of every line.
+-parcel is giving the faster devlopemnt experience bcz of caching.
+-Image Optimization
+-Minification of our file
+-Bundling 
+-Compressing
+-consistent Hashing
+-Code Splitting
+-Differential Bundling-to support older browers
+-Error Handling
+-Tree shaking Algo-remove unused code 
+
+
+# Why ur app is Fast?
+It is because of react but not just react it is parcel/webpack/wheat. Parcel is even using so many liberaries.
+
+# How parcel is fast?
+bcz it has a parcel-cache.It creates a different build and production
+
+
+# How to create a prod build?
+
+npx parcel build index.html but it will give an error so we have remove "main":"App.js" from package.json file
+
+Anything which generates automatically does not need to push to git.
+
+# How to execute the index.html file in parcel
+
+npx parcel index.html
+
+# How to run a project in development mode?
+"start":"parcel index.html",
+    "build":"parcel build index.html",
+    [Write this inside scripts]
+    and to run the script "npm run start"
+    And to build the project "npm run build"
+
+
